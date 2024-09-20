4da82bf795f59e7c57cbcdb73cd9272317468525
laxsjo <rasmus.soderhielm@gmail.com>
2024-09-20 15:20:56 +0200

    Add rustup for rust-analyzer to work in VSCode

--- home.nix
+++ home.nix
@@ -34,6 +34,7 @@
     man-pages-posix
     wayland-utils # To be able to use wayland-info in Info Center
     wineWowPackages.waylandFull # To be able to run windows EXEs.
+    rustup # To make the rust-analyzer vscode extension work.
     
     ## GUI utils
     kdePackages.filelight
