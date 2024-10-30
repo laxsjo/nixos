b04260f11e81bc3fc80cc94c9f44324d34af8862
laxsjo <rasmus.soderhielm@gmail.com>
2024-10-30 10:42:18 +0100

    Disable config permission check in openssh
    Added a patch which makes openssh disable the check for the permission
    of ~/.ssh/config, which is set incorrectly by home manager.

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -3,6 +3,15 @@
 let
   name = "ssh";
   cfg = config.module.${name};
+  # Due to home manager shenanigans the symlink to ~/.ssh/config has incorrect permissions
+  # (lrwxrwxrwx), which causes ssh inside FHS environment (such as in my VS Code setup) to
+  # fail with 'Bad owner or permissions on /home/rasmus/.ssh/config'. This patch disables
+  # that check in openssh.
+  # source: https://github.com/nix-community/home-manager/issues/322#issuecomment-1178614454
+  patchedOpenssh = pkgs.openssh.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [ ./openssh.patch ];
+    doCheck = false;
+  });
 in {
   options = {
     module.${name}.enable = lib.mkEnableOption "ssh. You want to enable this. (Why did I even make this an option?)";
@@ -11,6 +20,8 @@ in {
   config = lib.mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      package = patchedOpenssh;
+      
       includes = [
         "${./config}"
       ];
