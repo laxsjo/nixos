8d180bbcfa055849fb26629ec3dc3b140a333a08
laxsjo <rasmus.soderhielm@gmail.com>
2025-05-17 15:42:31 +0200

    Add twig-topiary to trusted substituters
    God I fucking hate the Nix substituters system...

---
+++ system-modules/cachix/tweag-topiary.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://tweag-topiary.cachix.org"
+      ];
+      trusted-public-keys = [
+        "tweag-topiary.cachix.org-1:8TKqya43LAfj4qNHnljLpuBnxAY/YwEBfzo3kzXxNY0="
+      ];
+    };
+  };
+}
