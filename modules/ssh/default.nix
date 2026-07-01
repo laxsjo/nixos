14c5596c98699f546ede3e699cf7a3b65b538a03
laxsjo <rasmus.soderhielm@gmail.com>
2026-07-01 09:36:10 +0200

    Inline default SSH config
    Explicitly adds the default SSH configuration, which is they say will be
    removed in the future.

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -29,6 +29,20 @@ in
     programs.ssh = {
       enable = true;
       package = patchedOpenssh;
+      
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
 
       includes = [
         "${./config}"
